b601258558be25990367bf0ebc21b8896672e820
Administrator <Administrator@WIN-S9E1GO3BERK>
2017-03-29 09:40:47 +0800

    executor

--- src/main/java/com/springboot/spring4/executor/ExecutorConfig.java
+++ src/main/java/com/springboot/spring4/executor/ExecutorConfig.java
@@ -1,5 +1,38 @@
 package com.springboot.spring4.executor;
 
-public class ExecutorConfig {
+import java.util.concurrent.Executor;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@ComponentScan("com.springboot.spring4.executor")
+@EnableAsync//1 开启异步任务支持
+public class ExecutorConfig implements AsyncConfigurer{
+
+	/**
+	 * 重写 getAsyncExecutor 返回一个ThreadPoolTaskExecutor
+	 * 获取到了基于线程池的 executor
+	 */
+	@Override
+	public Executor getAsyncExecutor() {
+		// TODO Auto-generated method stub
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(5);
+		taskExecutor.setMaxPoolSize(10);
+		taskExecutor.setQueueCapacity(25);
+		taskExecutor.initialize();
+		return taskExecutor;
+	}
+
+	@Override
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
